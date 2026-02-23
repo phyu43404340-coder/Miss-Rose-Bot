c664a2e930fc4c16d032b50029cea1d6bafdaaff
phyu43404340-coder <phyu43404340@gmail.com>
2026-02-24 05:03:15 +0630

    Update config.py

--- tg_bot/config.py
+++ tg_bot/config.py
@@ -8,8 +8,8 @@ class Development(Config):
     OWNER_ID = 6440394225
     OWNER_USERNAME = "logic_dex"
     
-    # RECOMMENDED
-    SQLALCHEMY_DATABASE_URI = "postgresql://avnadmin:AVNS_y6q98elxsgXzYloNLMB@pg-14067a43-phyu43404340-1cc5.c.aivencloud.com:17798/defaultdb?sslmode=require"
+    # RECOMMENDED - SQLite ကိုပြောင်းထားပါတယ်
+    SQLALCHEMY_DATABASE_URI = "sqlite:///missrose.db"
     MESSAGE_DUMP = -1003529420898
     
     # OPTIONAL
@@ -18,7 +18,7 @@ class Development(Config):
     WHITELIST_USERS = []
     DEL_CMDS = True
     STRICT_GBAN = True
-    WORKERS = 2  # ဒီမှာ 2 ကိုပြောင်းထားတယ်
+    WORKERS = 2
     ALLOW_EXCL = True
     BMERNU_SCUT_SRELFTI = None
     
